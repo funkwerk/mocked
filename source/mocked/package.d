7445e91a9a2eb7f9c73b7a6e9f4105cc76e49772
Eugen Wissner <belka@caraus.de>
2020-07-28 11:54:59 +0200

    Revert making Mock a static class
    Since mocking nested classes doesn't work anyway, Mock can be a nested
    class injected with a mixin template. It gives better error messages
    since the class doesn't need a bunch of template parameters.
    
    Make Stubbed!T and Mocked!T take T again and not Mock!T, so they can be
    used publically in the application tests.

--- source/mocked/package.d
+++ source/mocked/package.d
@@ -1,7 +1,6 @@
 module mocked;
 
-public import mocked.builder : Mocked, Stubbed;
 public import mocked.error : UnexpectedCallError, UnexpectedArgumentError,
        OutOfOrderCallError, ExpectationViolationException;
-public import mocked.mocker;
+public import mocked.mocker : configure, Factory, Mocked, Mocker, Stubbed;
 public import mocked.option : Comparator;
